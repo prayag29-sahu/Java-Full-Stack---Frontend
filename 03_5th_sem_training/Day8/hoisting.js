92a7abac75e3ac688e4cc7df13383a8d7bdb7bf7
Prayag Sahu <sahuprayag229@gmail.com>
2025-08-29 21:38:52 +0530

    hoisting js

---
+++ 03_5th_sem_training/Day8/hoisting.js
@@ -0,0 +1,19 @@
+// agr apne value ko pehle use kr liya ha usme bad declare or initialze krte ha to hoisting ke concept se ye value ko phle top of body declare kr leta ha phir baki exicution means = var a -> consloe.log(a) -> a=5 so o/p undefine 
+console.log(a)
+var a = 5;  // undefine
+// let a=5; // ReferenceError: Cannot access 'a' before initialization means agr valur undefine h to ue let or const me access nhi deta ye case sirf hoisting ke concept me apply hoga 
+
+
+
+// it treat as normal
+// var sum = () => {
+//     console.log("hqy")
+// }
+// sum() // hqy
+
+
+// o/p sum is not a function
+sum()
+var sum = () => {
+    console.log("hqy")
+}
