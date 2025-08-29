a5e038f745117e9ec19ca2457c5b20744b0714b0
Prayag Sahu <sahuprayag229@gmail.com>
2025-08-29 21:44:37 +0530

    Hoisting Function

---
+++ 03_5th_sem_training/Day8/hoistingInFunction.js
@@ -0,0 +1,13 @@
+// it treat as normal
+// var sum = () => {
+//     console.log("hqy")
+// }
+// sum() // hqy
+
+
+// o/p sum is not a function
+sum()
+var sum = () => {
+    console.log("hqy")
+}
+// sum() // TypeError: sum is not a function
