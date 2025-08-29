9c72300073306a90b95d10942fd5bab1ddfa5907
Prayag Sahu <sahuprayag229@gmail.com>
2025-08-29 21:46:59 +0530

    Hoisting in Var

---
+++ 03_5th_sem_training/Day8/hoistingInVar.js
@@ -0,0 +1,3 @@
+// agr apne value ko pehle use kr liya ha usme bad declare or initialze krte ha to hoisting ke concept se ye value ko phle top of body declare kr leta ha phir baki exicution means = var a -> consloe.log(a) -> a=5 so o/p undefine 
+console.log(a)
+var a = 5;  // undefine
