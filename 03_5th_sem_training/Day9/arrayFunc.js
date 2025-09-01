4058545ed77de07ac1710ceb8bfdd01d426590f4
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-01 11:09:26 +0530

    array functions

---
+++ 03_5th_sem_training/Day9/arrayFunc.js
@@ -0,0 +1,10 @@
+let a = [1, 2, 4, 6, 34, 56]
+a.push(11) // at end
+console.log(a);
+a.pop();
+console.log(a); // at end
+a.shift() // at start delete
+console.log(a);
+a.unshift(102) // at start add
+console.log(a);
+
