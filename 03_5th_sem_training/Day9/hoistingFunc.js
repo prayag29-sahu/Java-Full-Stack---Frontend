778602f075b48a7a5ea317b4c3141a67d64e5438
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-01 11:11:23 +0530

    Hoisting Function

---
+++ 03_5th_sem_training/Day9/hoistingFunc.js
@@ -0,0 +1,27 @@
+
+
+
+let sum = () => {
+    console.log("hiii");
+};
+sum()
+// hiii
+
+// sum1()
+// let sum1 = () => {
+//     console.log("hiii");
+// }
+// Cannot access 'sum1' before initialization
+
+const sum2 = () => {
+    console.log("hiii");
+};
+sum2() // hiii
+
+
+sum3()
+const sum3   = () => {
+    console.log("hiii");
+}
+//  Cannot access 'sum3' before initialization
+
