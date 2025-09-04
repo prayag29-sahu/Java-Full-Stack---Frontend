c1a4457d65365ac0590441b8a96d00a456aa07bc
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-04 22:14:09 +0530

    timer synchro

--- 03_5th_sem_training/Days12/timersync.js
+++ 03_5th_sem_training/Days12/timersync.js
@@ -1,14 +1,28 @@
-function timerSync() {
+function timerSync(fun) {
     console.log("Timer started");
+    fun()
+}
+
+function timer(fun) {
     for (let i = 0; i <= 5; i++) {
 
         setTimeout(() => {
-            console.log(i);
+            console.log(i)
+            if (i === 5) {
+                fun(); // Call fun after the last number is printed
+            }
+;
         }, i * 1000)
     }
+}
+function timeout() {
     setTimeout(() => {
         console.log("Timer finished");
-    }, 6000);
+    }, 1000);
 }
 
-timerSync();
+timerSync(() => {
+    timer(() => {
+        timeout()
+    })
+});
