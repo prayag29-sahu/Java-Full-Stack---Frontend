847e9a043ee42d0f77b9cb6c078fafc0b1b56444
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-06 21:51:39 +0530

    functions in js

---
+++ 03_5th_sem_training/Day6/10_function.js
@@ -0,0 +1,12 @@
+
+function sayMyName() {
+    console.log("P");
+    console.log("R");
+    console.log("A");
+    console.log("Y");
+    console.log("A");
+    console.log("G");
+}
+
+sayMyName()
+sayMyName()
