75e14ed4f6078360d9be482a615a4c32e7166d2e
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-06 21:54:42 +0530

    get array values

---
+++ 03_5th_sem_training/Day6/15_getArrVal.js
@@ -0,0 +1,9 @@
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
