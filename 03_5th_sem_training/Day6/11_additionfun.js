f30b425b2933351da395a32d8e2564fbdeb966f8
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-06 21:51:55 +0530

    addition

---
+++ 03_5th_sem_training/Day6/11_additionfun.js
@@ -0,0 +1,17 @@
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2) {
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+// console.log("Result: ", addTwoNumbers(6, 9));
+
+const result = addTwoNumbers(3, 5)
+
+console.log("Result: ", result);
