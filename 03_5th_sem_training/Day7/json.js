a0b355c19403f80d3f8f2dbaa8a7af0cf68d32a5
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-06 21:39:02 +0530

    json file structure

---
+++ 03_5th_sem_training/Day7/json.js
@@ -0,0 +1,19 @@
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+console.log(users[1].id)
+console.log(users[1].email)
