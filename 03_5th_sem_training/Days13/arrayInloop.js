6825caeeee14190ca82d12c0dad523d0a696c5c9
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-08 15:01:04 +0530

    Array in loop

---
+++ 03_5th_sem_training/Days13/arrayInloop.js
@@ -0,0 +1,7 @@
+let myArray = ["flash", "batman", "superman"]
+//console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+
+}
