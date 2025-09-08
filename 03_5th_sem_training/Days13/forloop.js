f2f2a7d5c0fefb1866f95691702cae0834ea269c
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-08 15:01:53 +0530

    for loop

---
+++ 03_5th_sem_training/Days13/forloop.js
@@ -0,0 +1,12 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    // if (element == 5) {
+        //console.log("5 is best number");
+    // }
+    console.log(element);
+
+}
+
+// console.log(element);
