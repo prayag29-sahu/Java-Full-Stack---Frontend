8586f813877d7ffc4b4a024713b0e6e7935483d6
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-08 14:52:18 +0530

    nullish operator

---
+++ 03_5th_sem_training/Day9/nullishOpert.js
@@ -0,0 +1,11 @@
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+
+console.log(val1);
