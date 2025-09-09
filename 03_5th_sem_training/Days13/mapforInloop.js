7ed3413bf0c95b33a0dde5de661d4406a0964c76
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-09 13:25:30 +0530

    map in for in loop

---
+++ 03_5th_sem_training/Days13/mapforInloop.js
@@ -0,0 +1,10 @@
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map) {
+    console.log(key, ':-', map.get(key));
+}
