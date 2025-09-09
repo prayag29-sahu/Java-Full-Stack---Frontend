cc72388c5e5b1c81b9b03ec6c0ff5be0a40f1d7f
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-09 13:22:55 +0530

    do while loop

---
+++ 03_5th_sem_training/Days13/dowhile.js
@@ -0,0 +1,14 @@
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+let a = 11
+
+do {
+    console.log(`Score is ${a}`);
+    a++
+} while (a <= 20);
