7d4227fb4b835e0c820822a149bac2bdf20e3f71
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-09 13:26:11 +0530

    object in for of loop

---
+++ 03_5th_sem_training/Days13/objectforOfloop.js
@@ -0,0 +1,12 @@
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value);
+    
+}
+
+
+// TypeError: myObject is not iterable
