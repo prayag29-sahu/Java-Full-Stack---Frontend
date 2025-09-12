cab4b79194ee5c638d124399af2af4e9cce1f35e
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-12 18:58:45 +0530

    for each loop in object

---
+++ 03_5th_sem_training/Days15/forEachInObj.js
@@ -0,0 +1,19 @@
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach((item) => {
+
+    console.log(item.languageName);
+})
