9f0cc0ea581c0a84b47847741eb051e5cbc8dff4
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-17 18:05:47 +0530

    Array Destructuring

--- 03_5th_sem_training/Days22/ArrayDestructuring.js
+++ 03_5th_sem_training/Days22/ArrayDestructuring.js
@@ -1,3 +1,12 @@
 let arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+// let a = arr[0];
+// let b = arr[1];
+// let c = arr[2];
+// let d = arr[3];
+// let e = arr[4];  
+// console.log(a, b, c, d, e);
+
+
 let [a, b, c, d, e] = arr;
 console.log(a, b, c, d, e);
