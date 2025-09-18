a845f74be04dcc17475cd914f74c582cbd6d0cde
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-18 11:43:52 +0530

    Prototype creation

---
+++ 03_5th_sem_training/Days23/CreatePrototype.js
@@ -0,0 +1,17 @@
+
+function createUser(username, score) {
+    this.username = username
+    this.score = score
+}
+
+createUser.prototype.increment = function () {
+    this.score++
+}
+createUser.prototype.printMe = function () {
+    console.log(`price is ${this.score}`);
+}
+
+const chai = new createUser("chai", 25)
+const tea = createUser("tea", 250)
+
+chai.printMe()
