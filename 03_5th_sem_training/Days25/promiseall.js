f3e8cb14deddbe78bfb6356ca1511de44ae2c4f7
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-21 16:38:12 +0530

    Promise.all

---
+++ 03_5th_sem_training/Days25/promiseall.js
@@ -0,0 +1,16 @@
+
+let p1 = new Promise((res,rej)=>{
+    res()
+})
+let p2 = new Promise((res,rej)=>{
+    res()
+})
+
+let p3 = new Promise((res,rej)=>{
+    res()
+})
+
+Promise.all([p1,p2,p3]).then((res)=>{
+    console.log(res);
+    
+})
