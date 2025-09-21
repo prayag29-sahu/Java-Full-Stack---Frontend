ab75a32dedfd38db2d5fd6e51c923bb6d5c35fcb
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-21 16:37:43 +0530

    promise.allsettled func

---
+++ 03_5th_sem_training/Days25/promise.allsettled.js
@@ -0,0 +1,19 @@
+
+
+let p1 = new Promise((res,rej)=>{
+    res()
+})
+let p2 = new Promise((res,rej)=>{
+    res()
+})
+
+let p3 = new Promise((res,rej)=>{
+    res()
+})
+
+
+Promise.allSettled([p1,p2,p3]).then((res)=>{
+    console.log(res);
+    
+})
+
