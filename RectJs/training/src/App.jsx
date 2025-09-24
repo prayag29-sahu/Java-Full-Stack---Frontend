c750a154a99a58712c48268a6ce456fe9aeb3d21
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-24 20:56:45 +0530

    App file

--- RectJs/training/src/App.jsx
+++ RectJs/training/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 const App = () => {
   let [count, SetCount] = useState(0)
+  let [color, SetColor] = useState("red")
   let [state, SetState] = useState("Delhi")
   useEffect(() => {
     console.log("hii");
@@ -14,7 +15,9 @@ const App = () => {
     <div>
       <h1>{count}</h1>
       <h1>{state}</h1>
+      <h1>{color}</h1>
       <button onClick={()=>SetCount(count+1)}>click</button>
+      <button onClick={()=>SetColor("yellow")}>color</button>
     </div>
   )
 }
