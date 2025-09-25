062cb3f7269a97954444e5747d1e1085f249e9a3
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-25 10:21:03 +0530

    App jsx

--- RectJs/training/src/App.jsx
+++ RectJs/training/src/App.jsx
@@ -1,24 +1,29 @@
-import React, { useState, useEffect } from 'react'
+// import React, { useState, useEffect } from 'react'
+import API from "../src/components/Api"
+import "./App.css"
 
 const App = () => {
-  let [count, SetCount] = useState(0)
-  let [color, SetColor] = useState("red")
-  let [state, SetState] = useState("Delhi")
-  useEffect(() => {
-    console.log("hii");
-  }, [state]);
+  // let [count, SetCount] = useState(0)
+  // let [color, SetColor] = useState("red")
+  // let [state, SetState] = useState("Delhi")
+  // useEffect(() => {
+  //   console.log("hii");
+  // }, [state]);
 
   
 
   
   return (
-    <div>
+    <>
+    {/* <div>
       <h1>{count}</h1>
       <h1>{state}</h1>
       <h1>{color}</h1>
       <button onClick={()=>SetCount(count+1)}>click</button>
       <button onClick={()=>SetColor("yellow")}>color</button>
-    </div>
+      </div> */}
+      <API />
+      </>
   )
 }
 
