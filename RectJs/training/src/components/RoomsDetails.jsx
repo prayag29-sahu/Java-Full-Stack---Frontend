859b897e16603a0a0ea4bf3e53f68c26483ee841
Prayag Sahu <sahuprayag229@gmail.com>
2025-09-26 11:50:06 +0530

    Rooms details

---
+++ RectJs/training/src/components/RoomsDetails.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+
+const RoomsDetails = () => {
+    let [apiData, SetData] = useState([])
+    useEffect(() => {
+        fetch("./Rooms.json").then((res) => {
+            return res.json()
+        }).then((data) => {
+            console.log(data.rooms);
+            SetData(data.rooms)
+        })
+    }, []);
+
+    function handleDelete(item) {
+        console.log(item);
+        let newarray = apiData.filter((a, b) => {
+            return b !== item
+        })
+        SetData(newarray)
+    }
+    return (
+        <div>
+            {apiData.map((a, index) => {
+                return (
+                    <>
+                        <div id="card">
+                            <img src={a.image} />
+                            <p>{a.name}</p>
+                            <button onClick={() => handleDelete(index)}>Delete</button>
+                        </div>
+                    </>
+                )
+            })}
+        </div>
+    )
+}
+
+export default RoomsDetails;
