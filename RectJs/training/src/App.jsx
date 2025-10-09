c893ed5d2c7a47710248c3416998b143a91ec7f3
Prayag Sahu <sahuprayag229@gmail.com>
2025-10-09 20:03:50 +0530

    App jsx page

--- RectJs/training/src/App.jsx
+++ RectJs/training/src/App.jsx
@@ -1,23 +1,47 @@
-// import React, { useState, useEffect } from 'react'
-import UseState from "../src/components/UseState"
-// import API from "../src/components/Api"
-import Rooms from "../src/components/RoomsDetails"
-import "./App.css"
-import RoomsDetails from "../src/components/RoomsDetails"
+// // import React, { useState, useEffect } from 'react'
+// import UseState from "../src/components/UseState"
+// // import API from "../src/components/Api"
+// import Rooms from "../src/components/RoomsDetails"
+// import "./App.css"
+// import RoomsDetails from "../src/components/RoomsDetails"
+// import Form from "./components/form"
+
+
+
+// const App = () => {
+//   // let [count, SetCount] = useState(0)
+//   // useEffect(() => {
+//   //   console.log("hii");
+//   // }, [state]);
+
+//   return (
+//     <>
+//       {/* <UseState /> */}
+//       {/* <API /> */}
+//       {/* <RoomsDetails /> */}
+//       <Form />
+//     </>
+//   )
+// }
+
+// export default App
+import React from 'react';
+import Form from "./components/form";
+import Signup from "./components/Signup";import Login from "./components/Login";
+import { Routes, Route } from 'react-router-dom';
+import Search from "./components/search"
 
 const App = () => {
-  // let [count, SetCount] = useState(0)
-  // useEffect(() => {
-  //   console.log("hii");
-  // }, [state]);
-
   return (
-    <>
-      {/* <UseState /> */}
-      {/* <API /> */}
-      <RoomsDetails />
-    </>
-  )
+    <div>
+      {/* <Routes>
+        <Route path='/' element={<Form />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+      </Routes> */}
+      <Search />
+    </div>
+  );
 }
 
-export default App
+export default App;
